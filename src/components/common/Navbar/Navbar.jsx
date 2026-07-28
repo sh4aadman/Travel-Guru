387d3c997735a24a918b912882e20c2caf2750e4
sh4aadman <sh4adman@gmail.com>
2026-07-29 01:23:43 +0600

    navbar updated

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -7,13 +7,19 @@ function Navbar() {
   return (
     <nav className="flex items-center justify-between">
       <figure>
-        <img className="" src={logo} alt="travel-guru-logo" />
+        <img className="w-24 h-12" src={logo} alt="travel-guru-logo" />
       </figure>
       <Searchbar />
-      <ul className="flex items-center">{links}</ul>
-      <NavLink to={"/auth/login"}>
-        <button>Login</button>
-      </NavLink>
+      <section className="flex items-center gap-14">
+        <ul className="font-secondary font-medium text-primary flex items-center gap-14">
+          {links}
+        </ul>
+        <NavLink to={"/auth/login"}>
+          <button className="px-7.5 py-3 font-secondary font-medium text-accent rounded-sm bg-secondary">
+            Login
+          </button>
+        </NavLink>
+      </section>
     </nav>
   );
 }
--- src/components/ui/Search bar/Searchbar.jsx
+++ src/components/ui/Search bar/Searchbar.jsx
@@ -1,23 +1,14 @@
+import { CiSearch } from "react-icons/ci";
+
 function Searchbar() {
   return (
-    <label className="input">
-      <svg
-        className="h-[1em] opacity-50"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-      >
-        <g
-          strokeLinejoin="round"
-          strokeLinecap="round"
-          strokeWidth="2.5"
-          fill="none"
-          stroke="currentColor"
-        >
-          <circle cx="11" cy="11" r="8"></circle>
-          <path d="m21 21-4.3-4.3"></path>
-        </g>
-      </svg>
-      <input type="search" required placeholder="Search" />
+    <label className="px-3.5 py-2.5 w-sm border border-primary rounded-sm flex items-center gap-3">
+      <CiSearch className="text-2xl" />
+      <input
+        className="w-full placeholder:font-secondary placeholder:font-medium placeholder:text-primary"
+        type="search"
+        placeholder="Search your destination"
+      />
     </label>
   );
 }
